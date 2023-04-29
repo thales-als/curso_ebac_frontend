00b17895c9651a1ebc28e8fd0c89182fc9aa72f9
thales-als <thalessouza.dev@gmail.com>
2023-04-28 23:02:57 -0300

    Exercício do módulo 7 - HTML e JS :)

---
+++ main.js
@@ -0,0 +1,19 @@
+const form = document.getElementById('form-numbers');
+const campoA = document.getElementById('campo-a');
+const campoB = document.getElementById('campo-b');
+let formValido = false;
+
+function validaNumero(campoA, campoB) {
+    return campoA < campoB;
+}
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault;
+
+    formValido = validaNumero(campoA.value, campoB.value);
+    if (formValido) {
+        alert('O formulário é válido!');
+    } else {
+        alert('O formulário não é válido!');
+    }
+});
