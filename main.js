67c1084ee48beb9a84b12ec0f3ef4af2bf6ad891
thales-als <thalessouza.dev@gmail.com>
2023-05-06 23:04:49 -0300

    Exercício do Módulo 10 - Plugins jQuery :)

---
+++ main.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+
+    $('form').on('submit', function (e) {
+        e.preventDefault();
+    });
+
+    $('#telefone').mask('(00) 00000-0000');
+    $('#cpf').mask('000.000.000-00');
+    $('#cep').mask('00000-000');
+
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true,
+            },
+            email: {
+                required: true,
+                email: true,
+            },
+            telefone: {
+                required: true,
+            },
+            cpf: {
+                required: true,
+            },
+            endereco: {
+                required: true,
+            },
+            cep: {
+                required: true,
+            },
+        },
+        submitHandler: function (form) {
+            console.log(form)
+        },
+        invalidHandler: function (evento, validador) {
+            let camposIncorretos = validador.numberOfInvalids();
+            if (camposIncorretos) {
+                alert(`Existem ${camposIncorretos} campos incorretos`);
+            }
+        }
+    });
+});
