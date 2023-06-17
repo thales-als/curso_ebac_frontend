f7e757d457733d8e3b59fbda9b34044ad7cf165c
thales-als <thalessouza.dev@gmail.com>
2023-06-17 14:46:18 -0300

    Corrigindo exercício do módulo 25

--- main.js
+++ main.js
@@ -1,15 +1,11 @@
-let alunos = [
+const alunos = [
     { nome: "João", nota: 5 },
     { nome: "Maria", nota: 4 },
     { nome: "Pedro", nota: 7 },
     { nome: "Ana", nota: 10 }
 ];
 
-const alunosAprovados = alunos.filter(filtrarAlunosPorNota)
-
-function filtrarAlunosPorNota(alunos) {
-    return alunos.nota >= 6;
-};
+const alunosAprovados = alunos.filter(aluno => aluno.nota >= 6);
 
 console.log(alunosAprovados);
 
